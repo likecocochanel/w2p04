c868b6ff48fbf4e409da84adb8c8a19a4c60fa1e
Elias <eliasvaldez9724@gmail.com>
2022-09-25 16:56:10 -0500

    Initial commit

---
+++ src/wp204.java
@@ -0,0 +1,16 @@
+public class wp204 {
+
+    public static void main(String[] args) {
+
+        String name = "Elias Valdez";
+        int age = 24;
+        double annualPay = 900000.00;
+        System.out.print("My name is ");
+        System.out.print(name);
+        System.out.print(", my age is ");
+        System.out.print(age);
+        System.out.print(" and I hope to earn $");
+        System.out.print(annualPay);
+        System.out.print(" per year.");
+    }
+}
