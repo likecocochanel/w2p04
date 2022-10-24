93b56704257b3872dd47b40b9038a8d02e4b61b1
elias <112049960+likecocochanel@users.noreply.github.com>
2022-10-23 22:45:39 -0500

    Create salary.kt

---
+++ salary.kt
@@ -0,0 +1,27 @@
+public class myClass
+
+{
+
+public static void main(String[] args) {
+
+String name = "Joe Mahoney";
+
+//name is string variable
+
+//Joe Mahoney is literal in variable name
+
+int age = 26;
+
+//age is int variable 26 as literal
+
+double annualPay = 100000.0;
+
+//annualPay is double variable 100000.0 as literal
+
+System.out.println("My name is "+name+", my age is 26 and"); //Output the values in given Format
+
+System.out.println("I hope to earn $"+annualPay+" per year.");
+
+}
+
+}
