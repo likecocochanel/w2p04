00b2d42338f7b5c19a89f51eb6fde4d9ed401dc0
elias <112049960+likecocochanel@users.noreply.github.com>
2022-11-29 16:09:55 -0600

    Create salesPrediction.kt

---
+++ salesPrediction.kt
@@ -0,0 +1,6 @@
+fun main() {
+    val totalSales = 4.6
+    var eastCoast = totalSales*.62
+    println("There are $$totalSales million sales with the East Coast division having $eastCoast million. ")
+  }
+}
